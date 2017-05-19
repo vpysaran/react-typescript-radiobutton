1630c997dd72338779e145845dfafcd41524743e
Saravanakumar C <vpysaran@gmail.com>
2017-05-19 18:02:42 +0530

    Create app.tsx

---
+++ src/components/app.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import RadioButton from './radio-button/radio-button';
+
+interface Props extends React.Props<App> {
+}
+const styles = {
+  block: {
+    maxWidth: 250
+  },
+  checkbox: {
+    marginBottom: 16
+  }
+};
+
+export default class App extends React.Component<Props, {}> {
+  public render() {
+    return (
+      <div>
+        <h1>React Components in Typescript</h1>
+        <h2>Radio Button</h2>
+        <div style={styles.block}>
+          <RadioBtn label="Simple" style={styles.checkbox}/>
+        </div>
+      </div>
+    );
+  }
+}
