b88fd68f16c77a305233ff3c14438824cd747b88
Saravanakumar C <vpysaran@gmail.com>
2017-05-22 19:35:44 +0530

    Update radio-button.tsx

--- src/components/radio-button/radio-button.tsx
+++ src/components/radio-button/radio-button.tsx
@@ -3,16 +3,16 @@ import * as React from 'react';
 interface Props extends React.Props<RadioButton> {
   name?: string;
   label: string;
-  value: string;  
+  value: string;
 }
 
 export default class RadioButton extends React.Component<Props, {}> {
   public render() {
-    const {label, value, name} = this.props;
-    const inputElm = (<label><input name={name} type="radio" label={label} value={value} />{label}</label>);
+    const {name, label, value} = this.props;
+    const inputElm = (<label><input type="radio" name={name} label={label} value={value} />{label}</label>);
     return (
       <div>
-        {inputElm}
+        {inputElm}         
       </div>
     );
   }
